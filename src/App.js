fd334db44e1dcd96e20f3f8b958e0f233be7f8fb
Richard Rico <rrico@localhoe.local>
2021-07-19 16:27:03 -0400

    removing clock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Clock from 'react-live-clock';
-
-
 import './App.css';
 
 function App() {
@@ -9,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello there!</h1>
       <h2> Welcome to my react page on amplify</h2>
-      <h3>The time is <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/East'} /></h3>
+
     
     </div>
   );
