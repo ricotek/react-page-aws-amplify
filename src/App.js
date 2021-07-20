cfcf417987adeba74ccbbb9ada12a093d9679a9d
Richard Rico <rrico@localhoe.local>
2021-07-20 15:00:56 -0400

    adding more hellos

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello there!</h1>
-      <h2> Welcome to my react page on amplify</h2>
-
-    
+      <h1>Hello there</h1>
+      <h1>Asalaam alaikum</h1>
+      <h1>Hola</h1>
+      <h2> Welcome to my react page on amplify</h2> 
     </div>
   );
 }
